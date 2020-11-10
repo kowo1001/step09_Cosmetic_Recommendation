ef1679766986b4353942c9194bbc2a58b4dcccc6
kowo1001 <kowo1001@dongguk.eud>
2020-11-11 04:37:26 +0900

    change format

--- step09_totalReview/src/view/EndView.java
+++ step09_totalReview/src/view/EndView.java
@@ -2,6 +2,8 @@
 package view;
 
 import sale.domain.Cosmetic;
+import sale.domain.Customer;
+
 import java.util.ArrayList;
 
 public class EndView {
@@ -11,16 +13,38 @@ public class EndView {
 			System.out.println("화장품: " + cosmetic[i].getCosmetic() + " / " + "피부타입 : " + cosmetic[i].getSkin_type() + " / " + "가격 : " + cosmetic[i].getCosmetic_price() + "원");
 		}
 	}
+	
 	public static void printCosmetics() {
 		ArrayList al = new ArrayList(); 
+		ArrayList al2 = new ArrayList();
+		
 		al.add("지성"); 
 		al.add("건성");
 		al.add("예민"); 
-		al.add("복합성"); 
+		al.add("복합성");
+		
+		al2.add("오휘미라클아쿠아 젤크림");
+		al2.add("뉴트로지나 하이드로 부스트 크림");
+		al2.add("닥터바이오 크림");
+		al2.add("라네즈베이직라이트 크림");
 		
 		for (int i = 0; i< al.size(); i++) {
-			System.out.println("화장품: " + al.get(i));
+			System.out.println( al.get(i)+"이면 " +al2.get(i)+"입니다.");
 		}
+	}
+	
+	public static void printCustomer(){
+		Customer c = Customer.builder().id("이정민").skin_type("건성").grade("vip").build();
+		System.out.println(c.toString());
+		
+		Customer c2 = Customer.builder().id("염아정").skin_type("지성").grade("silver").build();
+		System.out.println(c2.toString());
+		
+		Customer c3 = Customer.builder().id("장종욱").skin_type("복합성").grade("member").build();
+		System.out.println(c3.toString());
+		
+		Customer c4 = Customer.builder().id("최지수").skin_type("예민").grade("nonmember").build();
+		System.out.println(c4.toString());
 	}
 
 }
--- step09_totalReview/src/view/StartView.java
+++ step09_totalReview/src/view/StartView.java
@@ -10,49 +10,13 @@ import log.Log;
 import sale.domain.Customer;
 
 public class StartView {
-	//지성이면 지성 화장품
-	public static void printCosmetics() {
-		ArrayList al = new ArrayList(); 
-		ArrayList al2 = new ArrayList();
-		
-		al.add("지성"); 
-		al.add("건성");
-		al.add("예민"); 
-		al.add("복합성");
-		
-		al2.add("오휘미라클아쿠아 젤크림");
-		al2.add("뉴트로지나 하이드로 부스트 크림");
-		al2.add("닥터바이오 크림");
-		al2.add("라네즈베이직라이트 크림");
-		
-		for (int i = 0; i< al.size(); i++) {
-			System.out.println( al.get(i)+"이면 " +al2.get(i)+"입니다.");
-		}
-	}
-	
-	public static void main(String[] args) {
-	
-		BasicConfigurator.configure();
-		EndView.printCosmetics(Data_Cosmetic.getCosmetic());
-	
-		Log.getChoice();
-		//new Customer("jjw","0022","vip","combination skin"),new Cosmetic("닥터 바이오 크림","sensitive skin ",20400)
-		
-		Customer c = Customer.builder().id("이정민").skin_type("건성").grade("vip").build();
-		System.out.println(c.toString());
-		
-		Customer c2 = Customer.builder().id("염아정").skin_type("지성").grade("silver").build();
-		System.out.println(c2.toString());
-		
-		Customer c3 = Customer.builder().id("장종욱").skin_type("복합성").grade("member").build();
-		System.out.println(c3.toString());
-		
-		Customer c4 = Customer.builder().id("최지수").skin_type("예민").grade("nonmember").build();
-		System.out.println(c4.toString());
-		
-		printCosmetics();
-	}
 
 	
+	public static void main(String[] args) {
+		EndView.printCosmetics(Data_Cosmetic.getCosmetic()); //MVC pattern을 이용한 피부타입에 따른 화장품 및 화장품 가격 출력
+		EndView.printCosmetics(); // ArrayList를 이용한 피부 타입에 따른 화장품 추천 
+		Log.getChoice(); //Log를 이용한 화장품 추천 기록
+		EndView.printCustomer(); //Builder를 이용한 고객정보 리스트 출력
+	}
 }
 
